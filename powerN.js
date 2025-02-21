45fafef8ad240510303673a0c126446fc9a9dd85
Scott <Scottyjmt13@gmail.com>
2025-02-20 19:51:31 -0700

    complete powerN

---
+++ powerN.js
@@ -0,0 +1,19 @@
+//Given base and n that are both 1 or more, compute recursively (no loops)
+//the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+function powerN(base, n){
+  if (n === 1) return base;
+  else (n > 1) return base * powerN(base, n-1);
+}
+
+console.log(powerN(3, 1));
+console.log(powerN(3, 2));
+console.log(powerN(3, 3));
+console.log(powerN(2, 1));
+console.log(powerN(2, 2));
+console.log(powerN(2, 3));
+console.log(powerN(2, 4));
+console.log(powerN(2, 5));
+console.log(powerN(10, 1));
+console.log(powerN(10, 2));
+console.log(powerN(10, 3));
